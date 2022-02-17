39cd524e2a8613e59d9540e2dd4ecd196da3b5e4
OLATUNDE <olatundequadril@gmail.com>
2022-02-16 22:57:45 -0500

    adding places controller

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express()
 app.get('/', function(req, res){
     res.send("Hello Woeld")
 })
-
+app.use('/controller', require('./Controller/places.js') )
 app.get('*', function(req, res){
     res.status(404).send("<h1>404 Page</h1>")
 })
+
 
 app.listen(process.env.PORT)
