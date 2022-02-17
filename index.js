83918a8474eac7476840e192bbc9bc4ce0f4abdb
OLATUNDE <olatundequadril@gmail.com>
2022-02-16 22:24:00 -0500

    Adding 404 page route

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+    res.send("Hello Woeld")
+})
+
+app.get('*', function(req, res){
+    res.status(404).send("<h1>404 Page</h1>")
+})
+
+app.listen(process.env.PORT)
